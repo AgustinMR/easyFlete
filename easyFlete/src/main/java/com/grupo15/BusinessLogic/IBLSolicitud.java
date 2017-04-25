56e42f84387ba333ae6496c40fd8b0fbd1896fb8
bruno-full-name <bruno.carballido@gmail.com>
2017-04-24 22:03:34 -0300

    actualizar solicitud casi terminado

--- easyFlete/src/main/java/com/grupo15/BusinessLogic/IBLSolicitud.java
+++ easyFlete/src/main/java/com/grupo15/BusinessLogic/IBLSolicitud.java
@@ -6,7 +6,7 @@ import java.util.List;
 public abstract interface IBLSolicitud {
     
     public boolean addSolicitud(double latOrigen, double lonOrigen, double latDestino, double lonDestino, int peso, int volumen, String descripcion, String fecha, double precioMax, String email);
-    public boolean updateSolicitud(int id, double latOrigen, double lonOrigen, double latDestino, double lonDestino, int peso, int volumen, String descripcion, String fecha, double precioMax, String email);
+    public boolean updateSolicitud(int id, double latOrigen, double lonOrigen, double latDestino, double lonDestino, int peso, int volumen, String descripcion, String fecha, double precioMax);
     public boolean deleteSolicitud(int id);
     public Solicitud getSolicitud(int id);
     public List<Solicitud> getAllSolicitudes();
--- easyFlete/src/main/java/com/grupo15/DataAccessLayer/ISolicitud.java
+++ easyFlete/src/main/java/com/grupo15/DataAccessLayer/ISolicitud.java
@@ -2,13 +2,15 @@ package com.grupo15.DataAccessLayer;
 
 import com.grupo15.easyflete.Solicitud;
 import com.grupo15.easyflete.SolicitudCliente;
+import java.util.Date;
 import java.util.List;
 
 public abstract interface ISolicitud {
     
     public boolean addSolicitud(Solicitud s);
     public boolean addSolicitudCliente(SolicitudCliente sCli);
-    public boolean updateSolicitud(Solicitud s);
+    public boolean updateSolicitud(int id,double latitudOrigen, double longitudOrigen, double latitudDestino, double longitudDestino, double precioMax, Integer peso, Integer volumen, String descripcion);
+    public boolean updateSolicitudCliente(int id, Date fecha);
     public boolean deleteSolicitud(int id);
     public Solicitud getSolicitud(int id);
     public SolicitudCliente getSolicitudCli(int id);
