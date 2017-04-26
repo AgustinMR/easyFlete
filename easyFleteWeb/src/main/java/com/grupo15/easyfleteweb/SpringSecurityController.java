a34f242e97a4b1aa3e275c3b0d52224891d8d8c7
Agustin Maidana <agustin.maidana@fing.edu.uy>
2017-04-26 14:34:09 -0300

    Pagina de inicio de sesion + Jquery Lib

--- easyFleteWeb/src/main/java/com/grupo15/easyfleteweb/SpringSecurityController.java
+++ easyFleteWeb/src/main/java/com/grupo15/easyfleteweb/SpringSecurityController.java
@@ -1,8 +1,6 @@
 package com.grupo15.easyfleteweb;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +11,18 @@ public class SpringSecurityController extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/", "/*").authenticated().anyRequest().permitAll().and().formLogin().loginPage("/").permitAll().and().logout().permitAll().and().cors().and().csrf().disable();
+        //http.authorizeRequests().antMatchers("/", "/*").authenticated().anyRequest().permitAll().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll().and().cors().and().csrf().disable();
+        http
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().anonymous()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+            .logout().permitAll();
+
     }
 
     /*@Autowired
