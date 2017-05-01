c7a60d9ae08269adf8b89aeade7b79150428fe72
AgustinMR <agustinmr1995@gmail.com>
2017-05-01 17:43:38 -0300

    algdsgrf

---
+++ easyFleteWeb/src/main/java/com/grupo15/easyfleteweb/easyFleteAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package com.grupo15.easyfleteweb;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+@Component
+public class easyFleteAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication a) throws AuthenticationException {
+        try {
+            Content r = Request.Post("http://localhost:10070/autenticar")
+                .bodyForm(Form.form().add("email", a.getName()).add("pass", a.getCredentials().toString()).build())
+                .execute().returnContent();
+            if(new Gson().toJson(r, Content.class). == true){
+            
+            }
+            List<GrantedAuthority> grantedAuths = new ArrayList<>();
+            grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+            return new UsernamePasswordAuthenticationToken(username, password, grantedAuths);
+        } catch (IOException ex) {
+            Logger.getLogger(easyFleteAuthenticationProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> type) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
